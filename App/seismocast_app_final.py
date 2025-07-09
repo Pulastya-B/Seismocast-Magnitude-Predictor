c5f2ba17bc24aa0f88c0a6bf7b1cf152f77a61a3
Pulastya-B <pulastyabhagwat.linkedin@gmail.com>
2025-07-09 23:10:27 +0530

    Update seismocast_app_final.py

--- App/seismocast_app_final.py
+++ App/seismocast_app_final.py
@@ -47,7 +47,7 @@ st.set_page_config(page_title="SeismoCast", layout="centered")
 st.markdown("""
     <style>
     .stApp {
-        background-image: url('https://images.unsplash.com/photo-1607990281364-68e08b2abacc');
+        background-image: url('https://plus.unsplash.com/premium_photo-1715616257496-5e14778bbc0c?q=80&w=715&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
         background-size: cover;
         background-repeat: no-repeat;
         background-attachment: fixed;
